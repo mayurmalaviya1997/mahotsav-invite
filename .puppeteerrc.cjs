fb744e247bf31ba76f284d1442a69a773009a870
Mayur Malaviya <mmalaviya517@gmail.com>
2024-12-16 03:26:04 +0530

    Configuration

---
+++ .puppeteerrc.cjs
@@ -0,0 +1,11 @@
+const {join} = require('path');
+/**
+ * @type {import("puppeteer").Configuration}
+ */
+module.exports = {
+  // Download Chrome (default `skipDownload: false`).
+  chrome: {
+    skipDownload: false,
+  },
+  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+};
